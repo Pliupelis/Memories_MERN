a9f7a3a36085eb77d2f2deb7642977978d0751cf
pliupelis <pliupelis@inbox.lt>
2021-10-20 12:51:09 +0300

    connecting mongoose & routes

---
+++ server/controllers/posts.js
@@ -0,0 +1,8 @@
+import PostMsg from "../models/postMesg"
+
+export const getPosts = (req, res)=>{
+    res.send('works')
+}
+export const createPost = (req, res)=>{
+    res.send('post creation')
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import postRoutes  from './routes/posts.js'
 
 const app = express()
-//bodyparser
+  app.use(cors())
+
+  //bodyparser
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true }))
-  app.use(cors())
+app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://admin:slaptazodis@cluster0.uvq9s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=>
+        app.listen(PORT, ()=>
+         console.log(`running on ${PORT}`)))
+    .catch((err)=>
+console.log(err.message))
+
