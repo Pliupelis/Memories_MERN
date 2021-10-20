36bd5b2813fd433591009cb0b650646d4ff63f16
pliupelis <pliupelis@inbox.lt>
2021-10-20 14:08:46 +0300

    styling

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,22 @@
-import PostMsg from "../models/postMesg"
+import PostMsg from "../models/postMesg.js"
 
-export const getPosts = (req, res)=>{
-    res.send('works')
+export const getPosts = async (req, res)=>{
+    try {
+        const postMesgs = await PostMsg.find()
+        console.log(postMesgs);
+        res.status(200).json(postMesgs)
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
 }
-export const createPost = (req, res)=>{
-    res.send('post creation')
+export const createPost = async (req, res)=>{
+    const post = req.body
+    const newPost = new PostMsg(post)
+
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
 }
